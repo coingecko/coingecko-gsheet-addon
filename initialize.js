0beda6adf4828cd4dfb50b03f45777cdea665d6f
Ranhiru Cooray <ranhiru@gmail.com>
2018-08-09 16:48:35 +0800

    Attempt at removing comments that might detect unnessary oauth scopes

--- initialize.js
+++ initialize.js
@@ -23,10 +23,6 @@ function getSidebarHtml() {
  * Creates a menu entry in the Google Docs UI when the document is opened.
  * This method is only used by the regular add-on, and is never called by
  * the mobile add-on version.
- *
- * @param {object} e The event parameter for a simple onOpen trigger. To
- *     determine which authorization mode (ScriptApp.AuthMode) the trigger is
- *     running in, inspect e.authMode.
  */
 
 function onOpen(e) {
@@ -42,12 +38,6 @@ function onOpen(e) {
  * Runs when the add-on is installed.
  * This method is only used by the regular add-on, and is never called by
  * the mobile add-on version.
- *
- * @param {object} e The event parameter for a simple onInstall trigger. To
- *     determine which authorization mode (ScriptApp.AuthMode) the trigger is
- *     running in, inspect e.authMode. (In practice, onInstall triggers always
- *     run in AuthMode.FULL, but onOpen triggers may be AuthMode.LIMITED or
- *     AuthMode.NONE.)
  */
 function onInstall(e) {
   onOpen(e);
